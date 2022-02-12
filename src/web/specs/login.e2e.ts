ad95575d3324609c7d051cecf43a40ce4226527d
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-02-12 12:15:27 +0530

    migrated to async mode
    steps performed:
    command 1: npm install jscodeshift @wdio/codemod
    command 2: npx jscodeshift -t ./node_modules/@wdio/codemod/async --parser=tsx ./filepath
    
    verified and make type changes
    
    Issue noticed: chaining of functions doesn't work well.

--- src/web/specs/login.e2e.ts
+++ src/web/specs/login.e2e.ts
@@ -3,14 +3,16 @@ import LoginPage from '../pages/login.page';
 import SecurePage from '../pages/secure.page';
 
 describe('Login application', () => {
-    it('TC_001 : should login with valid credentials', () => {
-        LoginPage.openLoginPage().performLogin('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toExist();
-        expect(SecurePage.flashAlert).toHaveTextContaining(LoginConstants.LOGIN_SUCCESS_MSG);
+    it('TC_001 : should login with valid credentials', async () => {
+        await LoginPage.openLoginPage()
+        await LoginPage.performLogin('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toExist();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(LoginConstants.LOGIN_SUCCESS_MSG);
     });
 
-    it('TC_002 : should not login with invalid credentials', () => {
-        LoginPage.openLoginPage().performLogin('foor', 'baar');
-        expect(SecurePage.flashAlert).toHaveTextContaining(LoginConstants.LOGIN_FAILED_MSG);
+    it('TC_002 : should not login with invalid credentials', async () => {
+        await LoginPage.openLoginPage()
+        await LoginPage.performLogin('foor', 'baar');
+        await expect(SecurePage.flashAlert).toHaveTextContaining(LoginConstants.LOGIN_FAILED_MSG);
     });
 });
