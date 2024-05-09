abcc0f08e78d449d5b68aabb13f4c7f1ed07e690
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2024-05-09 17:12:19 +1000

    changed constant to reduce redundant name

--- tests/static/loginConstants.ts
+++ tests/static/loginConstants.ts
@@ -1,4 +1,4 @@
 export default class LoginConstants {
-    static LOGIN_SUCCESS_MSG = 'You logged into a secure area!';
-    static LOGIN_FAILED_MSG = 'Your username is invalid!';
+    static SUCCESS_MSG = 'You logged into a secure area!';
+    static FAILED_MSG = 'Your username is invalid!';
 }
